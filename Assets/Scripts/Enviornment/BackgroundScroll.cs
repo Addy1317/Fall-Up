8e246ba819380114991d57171478edfc17203887
Aditya Singh <s.addy1317@gmail.com>
2024-08-22 23:51:44 +0530

    feat: BackgroundScrolling
    - Background Scrolling

---
+++ Assets/Scripts/Enviornment/BackgroundScroll.cs
@@ -0,0 +1,39 @@
+#region Summary
+/// <summary>
+///  
+/// </summary>
+#endregion
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SlowpokeStudio.FallUp
+{
+    public class BackgroundScroll : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private float _scrollSpeed = 0.3f;
+
+        private string _mainTextue = "_MainTex";
+
+        private MeshRenderer _meshRenderer;
+
+        private void Awake()
+        {
+            _meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+        private void Update()
+        {
+            HandleBackgroundScrolling();
+        }
+
+        private void HandleBackgroundScrolling()
+        {
+            Vector2 offset = _meshRenderer.sharedMaterial.GetTextureOffset(_mainTextue);
+            offset.y += Time.deltaTime * _scrollSpeed;
+
+            _meshRenderer.sharedMaterial.SetTextureOffset(_mainTextue, offset);
+        }
+    }
+}
