1079e90325a190dd8101558ccb12c9aaf75b4c8b
Aditya Singh <s.addy1317@gmail.com>
2024-10-06 15:16:08 +0530

    feat: New Assets
    - Added new Assets
    - Background Scrolling Script

---
+++ Assets/Scripts/Enviornment/Background/BackgroundManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SlowpokeStudio.FallUp
+{
+    public class BackgroundManager : MonoBehaviour
+    {
+        [SerializeField] private float scrollSpeed = 0.5f; 
+
+        private Vector2 startPos;
+        private float height;
+
+        void Start()
+        {
+            startPos = transform.position;
+            height = GetComponent<SpriteRenderer>().bounds.size.y; 
+        }
+
+        void Update()
+        {
+            // Move the background vertically
+            float newPosition = Mathf.Repeat(Time.time * scrollSpeed, height);
+            transform.position = startPos + Vector2.up * newPosition;
+        }
+    }
+}
